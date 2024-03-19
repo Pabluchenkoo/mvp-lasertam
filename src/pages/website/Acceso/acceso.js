a47c86d64262b45bbd34ab9d38f9b035252159f6
Juan Sebastian Pedraza Perez <js.pedrazap1@uniandes.edu.co>
2024-03-19 11:57:42 -0500

    Cambios finales en el Login y Facturacion en temas de internacionalizacion

--- src/pages/website/Acceso/acceso.js
+++ src/pages/website/Acceso/acceso.js
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 import RegistroForm from "./RegistroForm";
 import { saveAs } from 'file-saver';
+import { FormattedMessage} from 'react-intl';
 
 const Acceso = (props) => {
     const [selectedRole, setSelectedRole] = useState("");
@@ -69,52 +70,50 @@ const Acceso = (props) => {
     };
 
     return (
-        <>
-            <div style={{ width: '300px', margin: 'auto', textAlign: 'center' }}>
-                <h1>{props.type === "signup" ? "Sign Up" : "Login"}</h1>
-                <br />
-                {props.type === "signup" && (
-                    <>
-                        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-                            <Card style={{ width: '10rem', marginRight: '10px' }} onClick={() => handleRoleSelect("administrador")}>
-                                <Card.Img variant="top" src="../../../assets/pexels-cottonbro-studio-6804099.jpg" />
-                                <Card.Body>
-                                    <Card.Title>Administrador</Card.Title>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '10rem' }} onClick={() => handleRoleSelect("usuario")}>
-                                <Card.Img variant="top" src="imagen_usuario.jpg" />
-                                <Card.Body>
-                                    <Card.Title>Usuario</Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <RegistroForm role={selectedRole} onSubmit={handleFormSubmit} />
-                    </>
-                )}
-                {props.type !== "signup" && (
-                    <Form onSubmit={handleSubmit(handleFormSubmit)} className="mb-3"> 
-                        <FloatingLabel controlId="floatingInput" label="Email address">
-                            <Form.Control type="email" placeholder="Email" {...register('email', { required: true })} />
-                            {errors.email && <p>This field is required</p>}
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="Password" className="mt-3">
-                            <Form.Control 
-                                type={showPassword ? "text" : "password"}
-                                placeholder="Password" 
-                                {...register('password', { required: true })}
-                            />
-                            {errors.password && <p>This field is required</p>}
-                        </FloatingLabel>
-                        <Button block={showPassword ? "true" : "false"} onClick={toggleShowPassword} variant="secondary" className="mt-3 me-2">
-                            {showPassword ? "Hide" : "Show"} Password
-                        </Button>
-                        <Button type="submit" variant="primary" className="mt-3">Login</Button>
-                        {existeUsuario === false && <p style={{ color: 'red' }}></p>}
-                    </Form>
-                )}
-            </div>
-        </>
+        <div style={{ width: '300px', margin: 'auto', textAlign: 'center' }}>
+            <h1>{props.type === "signup" ? <FormattedMessage id="registro.titulo_registro" /> : <FormattedMessage id="registro.titulo_ingreso" />}</h1>
+            <br />
+            {props.type === "signup" && (
+                <>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                        <Card style={{ width: '10rem', marginRight: '10px' }} onClick={() => handleRoleSelect("administrador")}>
+                            <Card.Img variant="top" src="../../../assets/pexels-cottonbro-studio-6804099.jpg" />
+                            <Card.Body>
+                                <Card.Title><FormattedMessage id="registro.rol_administrador" /></Card.Title>
+                            </Card.Body>
+                        </Card>
+                        <Card style={{ width: '10rem' }} onClick={() => handleRoleSelect("usuario")}>
+                            <Card.Img variant="top" src="imagen_usuario.jpg" />
+                            <Card.Body>
+                                <Card.Title><FormattedMessage id="registro.rol_usuario" /></Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <RegistroForm role={selectedRole} onSubmit={handleFormSubmit} />
+                </>
+            )}
+            {props.type !== "signup" && (
+                <Form onSubmit={handleSubmit(handleFormSubmit)} className="mb-3"> 
+                    <FloatingLabel controlId="floatingInput" label={<FormattedMessage id="registro.label_correo" />}>
+                        <Form.Control type="email" placeholder={<FormattedMessage id="registro.label_correo" />} {...register('email', { required: true })} />
+                        {errors.email && <p><FormattedMessage id="registro.mensaje_error" /></p>}
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingPassword" label={<FormattedMessage id="registro.label_contrasena" />} className="mt-3">
+                        <Form.Control 
+                            type={showPassword ? "text" : "password"}
+                            placeholder={<FormattedMessage id="registro.label_contrasena" />} 
+                            {...register('password', { required: true })}
+                        />
+                        {errors.password && <p><FormattedMessage id="registro.mensaje_error" /></p>}
+                    </FloatingLabel>
+                    <Button block={showPassword ? "true" : "false"} onClick={toggleShowPassword} variant="secondary" className="mt-3 me-2">
+                        {showPassword ? <FormattedMessage id="registro.boton_ocultar" /> : <FormattedMessage id="registro.boton_mostrar" />}
+                    </Button>
+                    <Button type="submit" variant="primary" className="mt-3">{<FormattedMessage id="registro.boton_ingresar" />}</Button>
+                    {existeUsuario === false && <p style={{ color: 'red' }}></p>}
+                </Form>
+            )}
+        </div>
     );
 };
 
