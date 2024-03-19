b91e8a0a79c66ea88f1d0520e4ea46f74fe5bd03
mestradag <m.estradag@uniandes.edu.co>
2024-03-19 13:27:44 -0500

    i18n Landing Page

--- src/pages/website/WhyUs/whyUs.js
+++ src/pages/website/WhyUs/whyUs.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
 
 const WhyUs = () => {
 
         return (
             <>
-                <h1>Por qué elegirnos</h1>
+                <h1><FormattedMessage id="why.title"/></h1>
             </>
         );
 }
